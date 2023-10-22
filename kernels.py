92cab4b24d0571d2574d18148cd3023f645347dc
Matt Jones <matthewrjones01@gmail.com>
2023-10-23 00:11:11 +0100

    add

---
+++ kernels.py
@@ -0,0 +1,10 @@
+import numpy as np 
+
+class kernels(hyps):
+
+    def __init__(hyps):
+    
+    def squared_exponential(self):
+
+    hyps[0] = sf2
+    hyps[1] = ll 
---
+++ models.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class gpr(GP):
+
+    def optimise():
+
+    return opt_gp
+
+    def predict(self, xtest):
+
+    return f_star
+
+
+
